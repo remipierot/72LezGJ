05ddea6af323d8d3f38d29f1865ccfc6e0afe188
Antoine DELMOTTE <antoine06.del@hotmail.fr>
2019-05-14 16:18:03 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ 72LezGJ/Assets/Scripts/PortalTeleport.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public GameObject linkedPortal;
+    public bool canTeleport = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "TPableObject")
+        {
+            if (canTeleport)
+            {
+                linkedPortal.GetComponent<PortalTeleport>().canTeleport = false;
+                other.transform.position = new Vector3(linkedPortal.transform.position.x, other.transform.position.y, linkedPortal.transform.position.z);
+                if (other.gameObject.tag == "TPableObject")
+                {
+                    other.GetComponent<Rigidbody>().AddForce(other.GetComponent<pushingBox>().direction * 0.00002f);
+                }
+            }
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "TPableObject")
+        {
+            if (canTeleport == false)
+            {
+                canTeleport = true;
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 72LezGJ/Assets/Scripts/pushingBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushingBox : MonoBehaviour
+{
+
+    Vector3 positionA;
+    Vector3 positionB;
+    public Vector3 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+        print(direction);
+    }
+
+}
