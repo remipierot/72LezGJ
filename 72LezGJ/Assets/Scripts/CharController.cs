de4ae3a0127b89fa4836c3fda315a90446fb104d
Remi <rfpierot91@gmail.com>
2019-05-14 20:18:25 +0200

    Restore `TriggerScript`

--- 72LezGJ/Assets/Scripts/CharController.cs
+++ 72LezGJ/Assets/Scripts/CharController.cs
@@ -15,7 +15,7 @@ public class CharController : MonoBehaviour
 	public GameObject RightWalk;
 	public GameObject DownWalk;
 	public GameObject LeftWalk;
-	//public TriggerScript Trigger;
+	public TriggerScript Trigger;
 
 	void Update()
 	{
@@ -70,7 +70,6 @@ public class CharController : MonoBehaviour
 				StartCoroutine(_ScaleUp());
 				SetLayerRecursively(gameObject, 10); //Set Layer to Large
 			}
-			/*
 			else if(!Trigger.inWater)
 			{
 				_ChangingSize = true;
@@ -78,7 +77,6 @@ public class CharController : MonoBehaviour
 				StartCoroutine(_ScaleDown());
 				SetLayerRecursively(gameObject, 11); //Set Layer to Small
 			}
-			*/
 		}
 
 		_Body.velocity = direction.normalized * Speed;
---
+++ 72LezGJ/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+	public bool inWater = false;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Water"))
+		{
+			inWater = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Water"))
+		{
+			inWater = false;
+		}
+	}
+}
