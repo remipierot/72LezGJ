49f411ad03f85db92cb1e1f394d1ab45275e7b96
Remi <rfpierot91@gmail.com>
2019-05-14 18:56:03 +0200

    First camera handler

---
+++ 72LezGJ/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+	public GameObject Character;
+
+	void Update()
+	{
+		Vector3 targetPosition = Character.transform.position;
+		targetPosition.y = transform.position.y;
+
+		transform.position = targetPosition;
+	}
+}
