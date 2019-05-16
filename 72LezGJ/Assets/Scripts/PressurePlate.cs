adfe129a1b0dcd7a009f326a3595fec33f545ef7
Remi <rfpierot91@gmail.com>
2019-05-16 12:56:08 +0200

    Fix pressure plate

--- 72LezGJ/Assets/Scripts/PressurePlate.cs
+++ 72LezGJ/Assets/Scripts/PressurePlate.cs
@@ -4,15 +4,26 @@ public class PressurePlate : MonoBehaviour
 {
 	public GameObject LinkedElement;
 	private int _NbGOOnPlate = 0;
+	private bool _PlayerRegistered = false;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		LinkedElement.SetActive(false);
-		_NbGOOnPlate++;
+
+		if (!_PlayerRegistered || other.tag != "Player")
+		{
+			if(other.tag == "Player")
+				_PlayerRegistered = true;
+
+			_NbGOOnPlate++;
+		}
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
+		if (other.tag == "Player")
+			_PlayerRegistered = false;
+
 		_NbGOOnPlate--;
 
 		if(_NbGOOnPlate == 0)
